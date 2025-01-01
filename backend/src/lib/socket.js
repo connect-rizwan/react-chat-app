ee45f7f16ea67f9cdd99ee4fcb386f3c3813da44
Rizwan Ali <rizwanali96960@gmail.com>
2025-01-01 14:15:47 +0500

    completed

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,13 +1,13 @@
-import { Server } from "socket.io";
-import http from "http";
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:3000"],
   },
 });
 
@@ -34,4 +34,5 @@ io.on("connection", (socket) => {
   });
 });
 
-export { io, app, server };
+export { app, io, server };
+
